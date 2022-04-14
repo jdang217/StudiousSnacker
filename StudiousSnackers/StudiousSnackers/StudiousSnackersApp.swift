336668591b39b19e2f33e5045d986b2a38447de1
Jason Dang <jdang@Jasons-MacBook-Pro.local>
2022-04-14 14:59:11 -0400

    initial files

---
+++ StudiousSnackers/StudiousSnackers/StudiousSnackersApp.swift
@@ -0,0 +1,20 @@
+//
+//  StudiousSnackersApp.swift
+//  StudiousSnackers
+//
+//  Created by Jason Dang on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct StudiousSnackersApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
