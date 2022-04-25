f60531a22af6890d90291b188b73b47b4ccab98c
Jason Dang <jdang@Jasons-MacBook-Pro.local>
2022-04-24 20:14:37 -0400

    core data

--- StudiousSnackers/StudiousSnackers/Persistence.swift
+++ StudiousSnackers/StudiousSnackers/Persistence.swift
@@ -8,49 +8,69 @@
 import CoreData
 
 struct PersistenceController {
+    /*
+     'shared' is the struct variable holding the instance reference of the newly created PersistenceController instance.
+     It is referenced in any project file as PersistenceController.shared
+     */
     static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-
-    let container: NSPersistentContainer
+    // Property of the PersistenceController.shared instance
+    let persistentContainer: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "StudiousSnackers")
+        
+        persistentContainer = NSPersistentContainer(name: "StudiousSnackers")
+        
         if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+            persistentContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.viewContext.automaticallyMergesChangesFromParent = true
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+        persistentContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // fatalError() causes the application to generate a crash log and terminate.
+                // You should not use this function in a shipping application,
+                // although it may be useful during development.
 
                 /*
                 Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
+                    - The parent directory does not exist, cannot be created, or disallows writing.
+                    - The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                    - The device is out of space.
+                    - The store could not be migrated to the current model version.
                 Check the error message to determine what the actual problem was.
                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
     }
+    
+    //---------------------------------
+    // MARK: ✳️ CoreData Save Operation
+    //---------------------------------
+
+    // Method of the PersistenceController.shared instance
+    // Called in any project file as PersistenceController.shared.saveContext()
+    func saveContext () {
+        /*
+         PersistenceController.shared.persistentContainer's property viewContext
+         holds the object reference of the NSManagedObjectContext
+         */
+        let managedObjectContext: NSManagedObjectContext = PersistenceController.shared.persistentContainer.viewContext
+        
+        // Check to see if managedObjectContext has any changes
+        if managedObjectContext.hasChanges {
+            do {
+                // Try to save the changes
+                try managedObjectContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
 }
+
--- StudiousSnackers/StudiousSnackers/StudiousSnackersApp.swift
+++ StudiousSnackers/StudiousSnackers/StudiousSnackersApp.swift
@@ -14,7 +14,7 @@ struct StudiousSnackersApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+
         }
     }
 }
